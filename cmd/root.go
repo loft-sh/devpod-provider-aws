3a4ec40d6b0c012df6234225e7a4a84849215b18
Luca Di Maio <luca.dimaio1@gmail.com>
2023-03-09 10:15:46 +0100

    fix: unify rootCmd with gcloud provider code
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,64 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+	"os/exec"
 
-// NewAwsCmd returns a new root command
-func NewAwsCmd() *cobra.Command {
+	"github.com/loft-sh/devpod/pkg/log"
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/ssh"
+)
+
+// NewRootCmd returns a new root command
+func NewRootCmd() *cobra.Command {
 	awsCmd := &cobra.Command{
-		Use:   "aws",
-		Short: "aws Provider commands",
+		Use:           "devpod-provider-aws",
+		Short:         "aws Provider commands",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+
+		PersistentPreRunE: func(cobraCmd *cobra.Command, args []string) error {
+			log.Default.MakeRaw()
+			return nil
+		},
 	}
 
-	awsCmd.AddCommand(NewInitCmd())
-	awsCmd.AddCommand(NewCreateCmd())
-	awsCmd.AddCommand(NewDeleteCmd())
-	awsCmd.AddCommand(NewCommandCmd())
-	awsCmd.AddCommand(NewStartCmd())
-	awsCmd.AddCommand(NewStopCmd())
-	awsCmd.AddCommand(NewStatusCmd())
-
 	return awsCmd
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	// build the root command
+	rootCmd := BuildRoot()
+
+	// execute command
+	err := rootCmd.Execute()
+	if err != nil {
+		if exitErr, ok := err.(*ssh.ExitError); ok {
+			os.Exit(exitErr.ExitStatus())
+		}
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if len(exitErr.Stderr) > 0 {
+				log.Default.ErrorStreamOnly().Error(string(exitErr.Stderr))
+			}
+			os.Exit(exitErr.ExitCode())
+		}
+
+		log.Default.Fatal(err)
+	}
+}
+
+// BuildRoot creates a new root command from the
+func BuildRoot() *cobra.Command {
+	rootCmd := NewRootCmd()
+
+	rootCmd.AddCommand(NewInitCmd())
+	rootCmd.AddCommand(NewCreateCmd())
+	rootCmd.AddCommand(NewDeleteCmd())
+	rootCmd.AddCommand(NewCommandCmd())
+	rootCmd.AddCommand(NewStartCmd())
+	rootCmd.AddCommand(NewStopCmd())
+	rootCmd.AddCommand(NewStatusCmd())
+	return rootCmd
 }
