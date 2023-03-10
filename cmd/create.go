cb701e7d33822640d243206a42f6474c118bb39b
Luca Di Maio <luca.dimaio1@gmail.com>
2023-03-10 17:58:20 +0100

    fix: remove status check in create
    Signed-off-by: Luca Di Maio <luca.dimaio1@gmail.com>

--- cmd/create.go
+++ cmd/create.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/loft-sh/devpod-provider-aws/pkg/aws"
-	"github.com/loft-sh/devpod/pkg/client"
 	"github.com/loft-sh/devpod/pkg/log"
 	"github.com/loft-sh/devpod/pkg/provider"
 	"github.com/spf13/cobra"
@@ -34,14 +33,9 @@ func NewCreateCmd() *cobra.Command {
 
 // Run runs the command logic
 func (cmd *CreateCmd) Run(ctx context.Context, providerAws *aws.AwsProvider, machine *provider.Machine, logs log.Logger) error {
-	// check that we don't already have an instance
-	status, _ := aws.Status(providerAws.Session, providerAws.Config.MachineID)
-	if status == client.StatusNotFound {
-		// not found, let's create it
-		_, err := aws.Create(providerAws.Session, providerAws)
-		if err != nil {
-			return err
-		}
+	_, err := aws.Create(providerAws.Session, providerAws)
+	if err != nil {
+		return err
 	}
 
 	return nil
